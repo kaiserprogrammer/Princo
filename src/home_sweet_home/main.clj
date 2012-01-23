037c800da33386bf0026ff7abffb6a92da656e64
Jürgen Bickert <juergenbickert@gmail.com>
2012-01-23 06:35:55 +0100

    extract routing from main in own file

--- src/home_sweet_home/main.clj
+++ src/home_sweet_home/main.clj
@@ -1,7 +1,6 @@
 (ns home-sweet-home.main
   (:gen-class)
-  (:use home-sweet-home.core)
-  (:use [home-sweet-home.web])
+  (:use [home-sweet-home.routing])
   (:use ring.adapter.jetty)
   (:use [ring.util response])
   (:use [ring.middleware reload stacktrace params])
@@ -12,62 +11,9 @@
 (def db-path (.getCanonicalPath (File. "db")))
 (def db (FileSystemDB. db-path))
 
-(def get-request-handlers
-  {"/" {:controller (fn [req] {"Impressum" "/impressum"
-                              "Blog" "/blog"
-                              "Create" "/create"})
-        :presenter present-index-page}
-   "/impressum" {:interactor get-contact-information
-                 :presenter present-contact-information}
-   "/blog" {:interactor list-all-articles
-            :presenter present-all-articles}
-   "/article" {:interactor get-article
-               :controller #(Integer/parseInt (get (:params %) "id"))
-               :presenter present-blog}
-   "/create" {:presenter present-create-article}
-   "/edit" {:interactor get-article
-            :controller #(Integer/parseInt (or (get (:params %) "id") "-1"))
-            :presenter present-edit-article}})
-
-(defn edit-article-request [req]
-  {:id (Integer/parseInt (get (:params req) "id"))
-   :title (get (:params req) "new-title")
-   :content (get (:params req) "new-content")})
-
-(def post-request-handlers
-  {"/create" {:interactor save-article
-              :controller (fn [req]
-                          {:title (get (:params req) "title")
-                           :content (get (:params req) "content")})
-              :presenter present-save}
-   "/edit" {:interactor edit-article
-            :controller edit-article-request
-            :presenter (fn [res] (redirect (str "/article?id=" (:id res))))}})
-
-
-(defn handler-call [handler req]
-  (if-not (or (:interactor handler) (:controller handler))
-    ((:presenter handler) req)
-    (let [interactor (if-let [interactor (:interactor handler)]
-                       interactor
-                       (fn [req db] req))
-          controller (:controller handler)
-          presenter (:presenter handler)]
-      (if controller
-        (presenter (interactor (controller req) db))
-        (presenter (interactor db))))))
-
-(defn choose-handler [{:keys [uri request-method]} ]
-  (if (not (or (get-request-handlers uri)
-               (post-request-handlers uri)))
-    {:presenter present-request-information}
-    (if (= :post request-method)
-      (post-request-handlers uri)
-      (get-request-handlers uri))))
-
 (defn handle [req]
   (let [handler (choose-handler req)]
-    (handler-call handler req)))
+    (handler-call handler req db)))
 
 (def app
   (-> #'handle
---
+++ src/home_sweet_home/routing.clj
@@ -0,0 +1,55 @@
+(ns home-sweet-home.routing
+  (:use [home-sweet-home web core]))
+
+(defn edit-article-request [req]
+  {:id (Integer/parseInt (get (:params req) "id"))
+   :title (get (:params req) "new-title")
+   :content (get (:params req) "new-content")})
+
+
+(def get-request-handlers
+  {"/" {:controller (fn [req] {"Impressum" "/impressum"
+                              "Blog" "/blog"
+                              "Create" "/create"})
+        :presenter present-index-page}
+   "/impressum" {:interactor get-contact-information
+                 :presenter present-contact-information}
+   "/blog" {:interactor list-all-articles
+            :presenter present-all-articles}
+   "/article" {:interactor get-article
+               :controller #(Integer/parseInt (get (:params %) "id"))
+               :presenter present-blog}
+   "/create" {:presenter present-create-article}
+   "/edit" {:interactor get-article
+            :controller #(Integer/parseInt (or (get (:params %) "id") "-1"))
+            :presenter present-edit-article}})
+
+(def post-request-handlers
+  {"/create" {:interactor save-article
+              :controller (fn [req]
+                          {:title (get (:params req) "title")
+                           :content (get (:params req) "content")})
+              :presenter present-save}
+   "/edit" {:interactor edit-article
+            :controller edit-article-request
+            :presenter redirect-to-article}})
+
+(defn choose-handler [{:keys [uri request-method]} ]
+  (if (not (or (get-request-handlers uri)
+               (post-request-handlers uri)))
+    {:presenter present-request-information}
+    (if (= :post request-method)
+      (post-request-handlers uri)
+      (get-request-handlers uri))))
+
+(defn handler-call [handler req db]
+  (if-not (or (:interactor handler) (:controller handler))
+    ((:presenter handler) req)
+    (let [interactor (if-let [interactor (:interactor handler)]
+                       interactor
+                       (fn [req db] req))
+          controller (:controller handler)
+          presenter (:presenter handler)]
+      (if controller
+        (presenter (interactor (controller req) db))
+        (presenter (interactor db))))))
